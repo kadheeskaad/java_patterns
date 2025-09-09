3d33b93feb3a885203d54b69f17e541ea7256d72
kadheeskaad <kadheeskaabalan@gmail.com>
2025-09-09 19:27:00 +0530

    Add files via upload

---
+++ Upper_triangle_1.java
@@ -0,0 +1,19 @@
+class Upper_triangle_1
+{
+    public static void main(String[] args)
+    {
+        int n=4;
+        int i,j,k;
+        for(i=1;i<=n;i++)
+        {
+            for(j=1;j<=n-i;j++)
+            {
+                System.out.print(" ");
+            }
+            for(k=1;k<=(2*i-1);k++){
+                System.out.print("1");
+            }
+            System.out.println();
+        }
+    }
+}
